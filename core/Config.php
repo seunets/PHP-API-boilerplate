44465b7dd81e9ef95492c21e941f079690be219e
Alexander Pereira Girald <alexander.girald@icloud.com>
2021-11-24 14:48:15 -0300

    Create Config.php

---
+++ core/Config.php
@@ -0,0 +1,13 @@
+<?php
+namespace Core;
+
+class Config
+{
+   const DBDRIVER = 'pgsql';
+   const DBHOST = '';
+   const DBNAME = '';
+   const DBUSER = '';
+   const DBPASSWORD = '';
+   const JWT_KEY = '';
+   const ISS = '';
+}
